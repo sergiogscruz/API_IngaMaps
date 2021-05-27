e96097a75f5d74a745ccb82f4b41d6ac01b04532
KevenLuanNSG <kevenluan2012@gmail.com>
2021-05-26 21:47:21 -0300

    Cadastrando comentário sobre local

--- src/main/java/com/tourism/tourism/comment/CommentController.java
+++ src/main/java/com/tourism/tourism/comment/CommentController.java
@@ -1,12 +1,17 @@
 package com.tourism.tourism.comment;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/comment")
 public class CommentController {
   @Autowired
   private CommentService commentService;
+
+  @PostMapping
+  public Comment commentLocal(@RequestParam Long localId,
+                           @RequestBody String comment) {
+    return commentService.commentLocal(localId, comment);
+  }
 }
